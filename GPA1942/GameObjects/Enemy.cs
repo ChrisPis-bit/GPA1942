31a9f07090a577dad1fe9c16ee6aa567a8e4677a
ChrisPis-bit <54893471+ChrisPis-bit@users.noreply.github.com>
2020-04-15 15:56:25 +0200

    Enemy added with bullet collision

---
+++ GPA1942/GameObjects/Enemy.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPA1942
+{
+    class Enemy : RotatingSpriteGameObject
+    {
+        //Speed used for normal enemies
+        protected Vector2 baseSpeed = new Vector2(0, 40);
+
+        public Enemy(Vector2 position, string assetName = "Enemy") : base(assetName)
+        {
+            this.position = position;
+            velocity = baseSpeed;
+        }
+    }
+}
