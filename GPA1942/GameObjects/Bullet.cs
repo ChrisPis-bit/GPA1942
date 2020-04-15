5879fa06021d5ff5f33c82fb4e6942a4abb0cd2c
ChrisPis-bit <54893471+ChrisPis-bit@users.noreply.github.com>
2020-04-15 15:53:45 +0200

    Added shootable Bullets for the Player

---
+++ GPA1942/GameObjects/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPA1942
+{
+    class Bullet : RotatingSpriteGameObject
+    {
+        public Bullet(Vector2 position) : base("Bullet")
+        {
+            Origin = new Vector2(sprite.Width / 2, sprite.Height / 2);
+            this.position = position;
+            velocity.Y = -500;
+        }
+    }
+}
