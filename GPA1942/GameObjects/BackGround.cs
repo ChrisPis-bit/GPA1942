bc25b039b1d36b95b7b7132868d33ab455d0c236
ChrisPis-bit <54893471+ChrisPis-bit@users.noreply.github.com>
2020-04-29 18:17:09 +0200

    moving background lines added

--- GPA1942/GameObjects/BackGround.cs
+++ GPA1942/GameObjects/BackGround.cs
@@ -10,14 +10,25 @@ namespace GPA1942.GameObjects
     class BackGround : GameObjectList
     {
         const int START_OBJECT_AMOUNT = 10,
-            OBJECT_SPAWN_CHANCE = 50;
+            OBJECT_SPAWN_CHANCE = 50,
+            BG_SPEEDLINE_XSPEED = 100;
 
-        GameObjectList objects;
+        GameObjectList objects,
+            speedLines;
 
         public BackGround() : base()
         {
-            Add(new SpriteGameObject("BackGround"));
+
+            Add(speedLines = new GameObjectList());
             Add(objects = new GameObjectList());
+
+            speedLines.Add(new SpriteGameObject("BackGround"));
+            speedLines.Add(new SpriteGameObject("BackGround"));
+            speedLines.Children[0].Position -= new Vector2(0, (speedLines.Children[0] as SpriteGameObject).Height);
+            foreach (SpriteGameObject speedLine in speedLines.Children)
+            {
+                speedLine.Velocity = new Vector2(0, BG_SPEEDLINE_XSPEED);
+            }
 
             Reset();
         }
@@ -26,9 +37,18 @@ namespace GPA1942.GameObjects
         {
             base.Update(gameTime);
 
-            if(GameEnvironment.Random.Next(0, OBJECT_SPAWN_CHANCE) == 0)
+            if (GameEnvironment.Random.Next(0, OBJECT_SPAWN_CHANCE) == 0)
             {
                 objects.Add(new BackgroundObject());
+            }
+
+            foreach(SpriteGameObject speedLine in speedLines.Children)
+            {
+
+                if(speedLine.BoundingBox.Y >= GameEnvironment.Screen.Y)
+                {
+                    speedLine.Position = new Vector2(0, 0 - speedLine.Height);
+                }
             }
         }
 
@@ -45,7 +65,7 @@ namespace GPA1942.GameObjects
 
                 bgObject.Position = new Vector2(GameEnvironment.Random.Next(0 - (int)(bgObject.BoundingBox.Width * bgObject.size), GameEnvironment.Screen.X + (int)(bgObject.BoundingBox.Width * bgObject.size)),
                     GameEnvironment.Random.Next(0 - (int)(bgObject.BoundingBox.Height * bgObject.size), GameEnvironment.Screen.Y + (int)(bgObject.BoundingBox.Height * bgObject.size)));
-                
+
             }
         }
     }
